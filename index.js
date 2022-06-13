341da80b30ca5941d8fbb9483eac6e823d9548f1
Alemba <alembakiragu@gmail.com>
2022-06-13 16:54:31 +0300

    commiting changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, confirmed) {
+    return drivers.filter(
+      (perfectMatch) => perfectMatch.toLowerCase() === confirmed.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, testString) {
+    return drivers.filter(
+      (perfectMatch) =>
+        perfectMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(drivers, driversName) {
+    return drivers.filter((record) => record.name === driversName);
+  }
